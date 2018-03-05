5a356e19b9857896d25f8213f838d16d61257e36
Akhil Sreekumar <akhilsreekumar29@gmail.com>
2018-03-05 20:00:35 +0530

    Temp url changed

--- sensors/urls.py
+++ sensors/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from sensors import views
 
 urlpatterns = [
-    url(r'^sensors/$', views.sensors),
-    url(r'^sensors/(?P<pk>[0-9]+)/$', views.sensors_get),
+    url(r'^sensors/temp/$', views.sensors),
+    url(r'^sensors/temp/(?P<pk>[0-9]+)/$', views.sensors_get),
 ]
